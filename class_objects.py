7a229a9adfcfa32007915a03ea470772db8f1172
Tushar More <125184538+tusharmore26@users.noreply.github.com>
2023-04-28 23:29:38 +0530

    Add files via upload

--- class_objects.py
+++ class_objects.py
@@ -1,35 +1,20 @@
 class myclass:
-    x=10
+    name="Tushar"
+    age=21
+    lname="More"
 
-print(myclass())
-
-
-#creating object
-class newclass:
-    x=10
-    y=10
-    z=x+y
-
-n1=newclass()
-print(n1.z)
-
-
-#the __init__() function
-class thisclass():
     def __init__(self,name,age):
         self.name=name
         self.age=age
 
     def __str__(self):
-            return f"{self.name}{self.age}"
+        return f"{self.name},{self.age}"    
 
-    def function(self):
-        print("Name is:"+self.name)
-        
-t1=thisclass("Tushar",21)
-print(t1)
-t1.function()
+    def myfun(self):
+        print(self.name,"is ",self.age,"years old.")
+        print(f"{self.name} is {self.age} years old.")
 
 
-
-
+m2=myclass("Tushar",21)
+print(m2)
+m2.myfun()
