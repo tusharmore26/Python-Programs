e1ae566bbf053dc8122d4ce0905a21e2f1464042
Tushar More <125184538+tusharmore26@users.noreply.github.com>
2023-05-01 14:57:49 +0530

    Add files via upload

---
+++ math_module.py
@@ -0,0 +1,18 @@
+import math
+
+x=dir(math)
+print(x)
+
+sqr=math.sqrt(200)
+print(sqr)
+
+pow=math.pow(5, 3)
+print(pow)
+
+
+# math functions
+min=min(12,34,6,68,32,21)
+print(min)
+
+max=max(21,24,45,67,9,21,21)
+print(max)
